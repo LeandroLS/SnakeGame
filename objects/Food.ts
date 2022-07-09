096d90be576f659937c6c88567ba944114e93774
Leandro Lima <llimasilva@proton.me>
2022-07-09 00:42:39 -0300

    Refactor Food.js to Food.ts

--- objects/Food.ts
+++ objects/Food.ts
@@ -1,31 +1,38 @@
-var Food = /** @class */ (function () {
-    function Food() {
-    }
-    Food.prototype.isColliding = function (positionX, positionY) {
+class Food {
+    canvasWidth: number;
+    canvasHeight: number;
+    randomNumberX: number;
+    randomNumberY: number;
+    context: any;
+    boxSize: any;
+
+    isColliding(positionX: number, positionY: number): boolean {
         if (positionX == this.randomNumberX && positionY == this.randomNumberY) {
             this.generateRandomFoodPosition();
             return true;
-        }
-        else {
+        } else {
             return false;
         }
-    };
-    Food.prototype.generateFood = function () {
+    }
+
+    generateFood(): void {
         this.context.fillStyle = "#ff5252";
         this.context.fillRect(this.randomNumberX, this.randomNumberY, this.boxSize, this.boxSize);
-    };
-    Food.prototype.generateRandomFoodPosition = function () {
+    }
+
+    generateRandomFoodPosition(): void {
         this.randomNumberX = Math.floor(Math.random() * this.canvasWidth);
-        var restOfDivisionX = this.randomNumberX % this.boxSize;
+        const restOfDivisionX = this.randomNumberX % this.boxSize;
         if (restOfDivisionX != 0) {
             this.randomNumberX -= restOfDivisionX;
         }
+
         this.randomNumberY = Math.floor(Math.random() * this.canvasHeight);
-        var restOfDivisionY = this.randomNumberY % this.boxSize;
+        const restOfDivisionY = this.randomNumberY % this.boxSize;
         if (restOfDivisionY != 0) {
             this.randomNumberY -= restOfDivisionY;
         }
-    };
-    return Food;
-}());
-var food = new Food();
+    }
+}
+
+let food = new Food();
